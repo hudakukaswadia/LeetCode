33cd40a549f3b83ec6300a7a07f0e6942f8eaa66
Huda Kukaswadia <48019342+hudakukaswadia@users.noreply.github.com>
2022-06-11 12:21:28 +0100

    Time: 3 ms (56.80%), Space: 8.3 MB (76.19%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,20 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Recursive
 class Solution {
-public:
-    void inOrder(TreeNode* root, vector<int> &res){
-        if (root == NULL) {
-            return;
-        }
-        inOrder(root -> left, res);
-        res.push_back(root -> val);
-        inOrder(root -> right, res);
-    }
-    
+public:    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        inOrder(root, res);
-        return res;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        vector<int> inorder;
+        while(true) {
+            if(node != NULL) {
+                st.push(node);
+                node = node -> left;
+            }
+            else {
+                if (st.empty() == true) {
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                inorder.push_back(node -> val);
+                node = node -> right;
+            }
+        }
+        return inorder;
     }
 };
