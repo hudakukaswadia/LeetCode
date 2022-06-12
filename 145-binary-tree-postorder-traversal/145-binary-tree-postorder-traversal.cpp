a059e47ffab1ed9475a53f3d974442e6febef822
Huda Kukaswadia <48019342+hudakukaswadia@users.noreply.github.com>
2022-06-12 21:49:32 +0100

    Time: 0 ms (100.00%), Space: 8.5 MB (37.46%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -10,23 +10,53 @@
  * };
  */
 
-// Recursive 
+// Iterative using 2 Stacks
 
 class Solution {
 public: 
-    void postOrder(TreeNode* root, vector<int> &res) {
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postOrder;
         if (root == NULL) {
-            return;
+            return postOrder;
         }
         
-        postOrder(root -> left, res);
-        postOrder(root -> right, res);
-        res.push_back(root -> val);
+        stack<TreeNode*> st1;
+        stack<TreeNode*> st2;
+        
+        st1.push(root);
+        
+        while (!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if (root -> left != NULL) {
+                st1.push(root -> left);
+            }
+            if (root -> right != NULL) {
+                st1.push(root -> right);
+            }
+        }
+        
+        while (!st2.empty()) {
+            postOrder.push_back(st2.top() -> val);
+            st2.pop();
+        }
+        return postOrder;
     }
-    
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> res;
-        postOrder(root, res);
-        return res;
-    }
+        
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
