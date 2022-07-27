83d509e259645ff1657161e35e5f21d7cc9d293f
Huda Kukaswadia <48019342+hudakukaswadia@users.noreply.github.com>
2022-07-27 10:51:01 +0100

    Time: 1 ms (51.06%), Space: 42.9 MB (5.01%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -15,36 +15,57 @@
  */
 
 // Time Complexity: O(N)
-// Space Complexity: O(2N)
+// Space Complexity: O(N)
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         Stack<TreeNode> st = new Stack<TreeNode>();
+//         List<Integer> postOrder = new ArrayList<Integer>();
+        
+//         if (root == null) {
+//             return postOrder;
+//         }
+        
+//         while(root != null || !st.isEmpty()) {
+            
+//             if (root != null) {
+//                 st.push(root);
+//                 root = root.left;
+//             } else {
+//                 TreeNode temp = st.peek().right;
+//                 if (temp == null) {
+//                     temp = st.peek();
+//                     st.pop();
+//                     postOrder.add(temp.val);
+//                     while (!st.isEmpty() && temp == st.peek().right) {
+//                         temp = st.peek();
+//                         st.pop();
+//                         postOrder.add(temp.val);
+//                     }
+//                 } else root = temp;
+//             }
+//         }
+//         return postOrder;
+//     }
+// }
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
 
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        Stack<TreeNode> st = new Stack<TreeNode>();
-        List<Integer> postOrder = new ArrayList<Integer>();
-        
+    public static void postOrder(TreeNode root, ArrayList<Integer> res) {
         if (root == null) {
-            return postOrder;
+            return;
         }
         
-        while(root != null || !st.isEmpty()) {
-            
-            if (root != null) {
-                st.push(root);
-                root = root.left;
-            } else {
-                TreeNode temp = st.peek().right;
-                if (temp == null) {
-                    temp = st.peek();
-                    st.pop();
-                    postOrder.add(temp.val);
-                    while (!st.isEmpty() && temp == st.peek().right) {
-                        temp = st.peek();
-                        st.pop();
-                        postOrder.add(temp.val);
-                    }
-                } else root = temp;
-            }
-        }
-        return postOrder;
+        postOrder(root.left, res);
+        postOrder(root.right, res);
+        res.add(root.val);
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        postOrder(root, res);
+        return res;
     }
 }
